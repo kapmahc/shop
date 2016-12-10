8a8e21d27382c3eb44909c62b5e0f8259b5882f7
Jitang Zheng <jitang.zheng@gmail.com>
2016-12-10 08:12:52 -0800

    sitemap

--- lib/shop.rb
+++ lib/shop.rb
@@ -1,7 +1,7 @@
 require 'shop/engine'
 
 module Shop
-  def Shop.dashboard(user)
+  def self.dashboard(user)
     links = [
         {label: 'shop.addresses.index.title', href: 'addresses_path'},
         {label: 'shop.orders.index.title', href: 'orders_path'},
@@ -26,5 +26,24 @@ module Shop
         label: 'shop.dashboard.title',
         links: links
     }
+  end
+
+
+  def self.sitemap
+    app = Shop::Engine.routes.url_helpers
+    links = []
+    Setting.languages.each do |l|
+
+      links << {url: app.hot_variants_path(locale:l)}
+      links << {url: app.latest_variants_path(locale:l)}
+      links << {url: app.comments_path(locale:l)}
+
+      links += Product.select(:id, :updated_at).map do |i|
+        {url:product_path(i), lastmod:i.updated_at}
+      end
+
+      links << {url: app.root_path(locale:l)}
+    end
+    links
   end
 end
